bb25daeaa3cbf82eb809ed6773849b517a2901b6
diehe <heixia@vip.qq.com>
2020-05-25 11:06:24 +0800

    支持本地切割视频

--- made.php
+++ made.php
@@ -2,13 +2,15 @@
 $file = $argv[1];
 //echo 'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \'/mnt/d/下载/'.$file.'\'';
 //exit;
-$avg = exec('ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \'/mnt/d/下载/'.$file.'\'');
+//$avg = exec('ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 '.$file.);
+$string = 'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 '.$file;
+$avg = exec($string);
 $avg = round($avg);
 for($i=300;$i<=$avg-500;$i+=mt_rand(60,300))
 {
 	$start = gmdate('H:i:s',$i);
 	$len = mt_rand(60,300);
-	$cmd = 'ffmpeg -y -i  \'/mnt/d/下载/'.$file.'\' -ss '.$start.' -t '.$len.' -vcodec copy -acodec copy '.$i.'.mp4';
+	$cmd = 'ffmpeg -y -i  '.$file.' -ss '.$start.' -t '.$len.' -vcodec copy -acodec copy '.$i.'.mp4';
 	echo $cmd;
 	exec($cmd);//exit;
 	//$item[] = array('start'=>$start,'len'=>$len);
